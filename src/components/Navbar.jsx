e8d34d73bf115a2a9b6da27320a13e0830cacd5e
Sam <enthunt.sh@gmail.com>
2024-06-22 17:45:39 -0400

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+
+export default function Navbar() {
+  return (
+    <>
+      <nav id="nav">
+        <h2>WEIGHTWORLD</h2>
+        <ul id="nav-links">
+          <li></li>
+        </ul>
+        <button type="button" id="join">JOIN US</button>
+      </nav>
+    </>
+  );
+}
