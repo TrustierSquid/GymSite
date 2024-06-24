64c0084f3f406e336d37e3909ce432bd29222fdf
Sam <enthunt.sh@gmail.com>
2024-06-23 21:15:09 -0400

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,32 @@
 export default function Footer(){
     return (
         <>
-            <h1>Hello</h1>
+            <footer id="footer">
+                <section className="footerSection">
+                    <ul>
+                        <h3>Links</h3>
+                        <br />
+                        <li>Memberships</li>
+                        <br />
+                        <li>About</li>
+                        <br />
+                        <li>Contact</li>
+                    </ul>
+                </section>
+                <section className="footerSection">
+                    <ul>
+                        <li>Socials</li>
+                        <br />
+                        <div id="socials">
+                            <img src="src/icon/001-instagram.png" alt="" />
+                            <img src="src/icon/002-twitter.png" alt="" />
+                            <br />
+                            <img src="src/icon/004-pinterest.png" alt="" />
+                            <img src="src/icon/003-social-media.png" alt="" />
+                        </div>
+                    </ul>
+                </section>
+            </footer>
         </>
     )
 }
